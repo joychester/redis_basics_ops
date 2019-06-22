7e0318b72f619630aafacaebf5c047d14611d5b3
Cheng Chi (@joychester) <joychester@gmail.com>
2019-06-21 23:48:33 -0700

    Update redis_basics.js

--- redis_basics.js
+++ redis_basics.js
@@ -34,12 +34,47 @@ const rpopFromLinkedList = async function(list) {
   return await client.rpopAsync(list);
 };
 
-const getCurrentTaskFromLinedList = async function(list) {
+const getCurrentTaskFromLinkedList = async function(list) {
   return await client.lindexAsync(list,-1);
 };
 
 const getTaskLength = async function(list) {
   return await client.llenAsync(list);
+};
+
+const isKeyExist = async function(key) {
+  return (await client.existsAsync(key) > 0);
+};
+
+const delLinkedList = async function(list) {
+  return await client.delAsync(list);
+};
+
+const setTaskToHash = async function(hash, field_name, field_val) {
+  return await client.hsetAsync(hash, field_name, field_val);
+};
+
+const getTaskFromHash = async function(hash, field_name) {
+  return await client.hgetAsync(hash, field_name);
+};
+
+const getAllFieldNameFromHash = async function(hash) {
+  return await client.hkeysAsync(hash);
+};
+
+const getAllFieldValFromHash = async function(hash) {
+  return await client.hvalsAsync(hash);
+};
+
+const getTaskLengthFromHash = async function(hash) {
+  return await client.hlenAsync(hash);
+};
+
+const delTaskFromHash = async function(hash, field_name) {
+  if (await client.hexists(hash, field_name)) {
+    return await client.hdel(hash, field_name);
+  }
+  return 0;
 };
 
 /*
@@ -68,14 +103,14 @@ const getTaskLength = async function(list) {
     console.log('task_size: ' + task_size);
     let task_added = await lpushToLinkedList(mylist, task);
     console.log('task_added_index: ' + task_added);
-    let current_task = await getCurrentTaskFromLinedList(mylist);
+    let current_task = await getCurrentTaskFromLinkedList(mylist);
     console.log('current_task: ' + current_task);
     let task_popped = await rpopFromLinkedList(mylist);
     console.log('task_popped: ' + task_popped);
     
     task_added = await lpushToLinkedList(mylist, task_obj);
     console.log('task_added_index: ' + task_added);
-    current_task = await getCurrentTaskFromLinedList(mylist);
+    current_task = await getCurrentTaskFromLinkedList(mylist);
     console.log('current_task: ' + current_task);
     task_popped = await rpopFromLinkedList(mylist);
     console.log('task_popped: ' + task_popped);
