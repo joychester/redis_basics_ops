02356f9bd5cd7baefa0fa7a81477a23e6febe3ad
Cheng Chi (@joychester) <joychester@gmail.com>
2019-06-25 00:11:48 -0700

    Update redis_another_test.js

--- redis_another_test.js
+++ redis_another_test.js
@@ -3,7 +3,9 @@
 const {
   quit,
   setFieldToHash,
-  getAllFieldsValFromHash
+  getAllFieldsValFromHash,
+  getAllFieldsNameFromHash,
+  getAllFieldsFromHash
   
 } = require('./redis_helper');
 
@@ -17,16 +19,31 @@ const server_status = 'server_status';
     workers.forEach( async(server_info) => {
       await setFieldToHash(server_status, server_info, 0);
     });
+    console.log(`Server Status in Redis : ${JSON.stringify(await getAllFieldsFromHash(server_status))}`);
+
+    let server_names = await getAllFieldsNameFromHash(server_status);
+    console.log(`All workers detail info: ${server_names}`);
     let field_vals = await getAllFieldsValFromHash(server_status);
-    console.log(field_vals);
+    console.log(`All workers current status: ${field_vals}`);
 
     // Mock worker status
-    await setFieldToHash(server_status, workers[0], 1);
+    console.log("Changing workers status...");
+    await setFieldToHash(server_status, workers[0], Math.round(Math.random()));
+    await setFieldToHash(server_status, workers[1], Math.round(Math.random()));
+    await setFieldToHash(server_status, workers[2], Math.round(Math.random()));
 
     // Check if there is any worker idle, ['1', '0', '0']
     field_vals = await getAllFieldsValFromHash(server_status);
-    console.log(field_vals);
+    console.log(`All workers current status: ${field_vals}`);
+    
 
+    let idle_worker_indx = field_vals.findIndex( val => {
+      return val === '0';
+    });
+    console.log(`Avaible worker index : ${idle_worker_indx}`);
+
+    let worker_selected = (idle_worker_indx > -1) ? server_names[idle_worker_indx] : 'nil' ;
+    console.log(`Avaible worker info : ${worker_selected}`);
   }
   finally {
     quit();
