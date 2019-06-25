5aef2501c90ef406565d993f2650276c11d747ab
Cheng Chi (@joychester) <joychester@gmail.com>
2019-06-25 15:11:02 -0700

    Update redis_another_test.js

--- redis_another_test.js
+++ redis_another_test.js
@@ -9,11 +9,15 @@ const {
   
 } = require('./redis_helper');
 
-let workers = ['1.1.1.1', '2.2.2.2', '3.3.3.3'];
 const server_status = 'server_status';
+const check_interval_ms = 5000;
+let workers = ['1.1.1.1', '2.2.2.2', '3.3.3.3'];
+let count = 0;
 
-(async() => {
+/*(async() => {
   try {
+    console.log(`Check Server status every ${check_interval_ms} ms...`);
+
     // Init task: set worker status to idle
     // 0: idle ; 1: busy ; 2: error
     workers.forEach( async(server_info) => {
@@ -49,3 +53,45 @@ const server_status = 'server_status';
     quit();
   }
 })();
+*/
+
+setInterval(async() => {
+  try {
+    console.log(`Check Server status every ${check_interval_ms} ms...`);
+
+    // Init task: set worker status to idle
+    // 0: idle ; 1: busy ; 2: error
+    workers.forEach( async(server_info) => {
+      await setFieldToHash(server_status, server_info, 0);
+    });
+    console.log(`Server Status in Redis : ${JSON.stringify(await getAllFieldsFromHash(server_status))}`);
+
+    let server_names = await getAllFieldsNameFromHash(server_status);
+    console.log(`All workers detail info: ${server_names}`);
+    let field_vals = await getAllFieldsValFromHash(server_status);
+    console.log(`All workers current status: ${field_vals}`);
+
+    // Mock worker status
+    console.log("Changing workers status...");
+    await setFieldToHash(server_status, workers[0], Math.round(Math.random()));
+    await setFieldToHash(server_status, workers[1], Math.round(Math.random()));
+    await setFieldToHash(server_status, workers[2], Math.round(Math.random()));
+
+    // Check if there is any worker idle, ['1', '0', '0']
+    field_vals = await getAllFieldsValFromHash(server_status);
+    console.log(`All workers current status: ${field_vals}`);
+    
+
+    let idle_worker_indx = field_vals.findIndex( val => {
+      return val === '0';
+    });
+    console.log(`Avaible worker index : ${idle_worker_indx}`);
+
+    let worker_selected = (idle_worker_indx > -1) ? server_names[idle_worker_indx] : 'nil' ;
+    console.log(`Avaible worker info : ${worker_selected}`);
+  }
+  finally {
+      count++;
+      console.log(`current loop: ${count}`);
+  }
+}, check_interval_ms);
