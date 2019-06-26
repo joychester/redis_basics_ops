645fde73f882ecc374c5dce03dfcbdc6be93aced
Cheng Chi (@joychester) <joychester@gmail.com>
2019-06-25 23:47:34 -0700

    Add files via upload

---
+++ add_sample_task.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    lpushToLinkedList, 
+    getTaskLength
+} = require("./redis_helper");
+
+const task_list = "mylist";
+
+setInterval( async() => {
+    let task_obj = {"mytask": Math.floor(Math.random()*10000)};
+    let task_added = await lpushToLinkedList(task_list, task_obj);
+    console.log('task_added_index: ' + task_added);
+    console.log(`current total tasks : ${await getTaskLength(task_list)}`);
+}, (Math.round(Math.random()*5000) + 10000));
